6ba76dc975b624e98af16b997208e1d860af834b
Rafael Camargo <hello@rafaelcamargo.com>
2021-06-19 21:50:01 -0300

    chore: initial commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverageFrom: ['./src/**/*.js'],
+  coverageReporters: ['html', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
+}
